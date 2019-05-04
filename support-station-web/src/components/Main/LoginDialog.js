3a7f8d8fa9a6e225f2d0bc623433bd3f85bc69fb
Junsuk Park <park64kr63@gmail.com>
2019-05-04 10:05:03 +0900

    # EDIT - Login Dialog decoration

--- support-station-web/src/components/Main/LoginDialog.js
+++ support-station-web/src/components/Main/LoginDialog.js
@@ -3,32 +3,38 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import {
-  Dialog, DialogContent, DialogContentText, Grid,
+  Dialog, DialogContent, DialogTitle, DialogContentText, Grid,
 } from '@material-ui/core';
 import GoogleLogin from 'react-google-login';
 
 const styles = {
-
+  dialogTitle: {
+    background: '#3a84ff',
+    marginBottom: '10px',
+  },
+  dialogTitleText: {
+    color: 'white',
+  },
 };
 
 class LoginDialog extends Component {
   state = {
-    maxWidth: 'sm',
+    maxWidth: 'xs',
     fullWidth: true,
-  }
+  };
 
   handleClose = () => {
     const { onClose, selectedValue } = this.props;
     onClose(selectedValue);
-  }
+  };
 
   handleListItemClick = (value) => {
     const { onClose } = this.props;
     onClose(value);
-  }
+  };
 
   render() {
-    const { open } = this.props;
+    const { open, classes } = this.props;
     const { maxWidth, fullWidth } = this.state;
 
     return (
@@ -39,19 +45,16 @@ class LoginDialog extends Component {
         onClose={this.handleClose}
         open={open}
       >
+        <DialogTitle className={classes.dialogTitle} id="max-width-dialog-title">
+          <span className={classes.dialogTitleText}>Login</span>
+        </DialogTitle>
         <DialogContent id="max-width-dialog-title">
           <DialogContentText>
-            <Grid
-              container
-              justify="center"
-              alignItems="center"
-              direction="column"
-              spacing={24}
-            >
+            <Grid container justify="center" alignItems="center" direction="column" spacing={24}>
               <Grid item xs={12}>
                 <GoogleLogin
                   clientId="155990669240-40kbd3r90bu3r4tpmqpvs2ciru9eg1la.apps.googleusercontent.com"
-                  buttonText="Login"
+                  buttonText="Login with Google"
                   onSuccess={this.handleListItemClick}
                   cookiePolicy="single_host_origin"
                 />
@@ -68,6 +71,7 @@ LoginDialog.propTypes = {
   open: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
   selectedValue: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(LoginDialog);
